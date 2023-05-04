1d474453de27a2437207986a4a9c756c045c42bd
Crowdbotics <team@crowdbotics.com>
2023-05-04 03:33:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "curly-surf-40439",
+  url: "https://curly-surf-40439.botics.co",
+  api: "https://curly-surf-40439.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
